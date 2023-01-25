10a5e888faebb0d0628a0fd05cc354dbc53db120
Bice93 <bianca.cerulo@hotmail.it>
2023-01-25 17:25:20 +0100

    Fix on the Sondaggio

--- src/org/lessons/java/security/Sondaggio.java
+++ src/org/lessons/java/security/Sondaggio.java
@@ -1,23 +1,41 @@
 package org.lessons.java.security;
 
+import java.util.Scanner;
+
 public class Sondaggio {
 	
 	public static void main(String[] args) {
 		
 		int totalNumberStudents;
-		int windowsStudents = 10;
-		int linuxStudents = 2;
-		int macOsStudents = 8;
-
+		int windowsStudents;
+		int linuxStudents;
+		int macOsStudents;
+		
+		Scanner inputStudent = new Scanner(System.in);
+		
+		//Inserisco gli studenti che usano windows
+		System.out.println("Quanti studenti usano Windows?");
+		windowsStudents = inputStudent.nextInt();
+		
+		//Inserisco gli studenti che usano linux
+		System.out.println("Quanti studenti usano Linux?");
+		linuxStudents = inputStudent.nextInt();
+		
+		//Inserisco gli studenti che usano macOs
+		System.out.println("Quanti studenti usano MacOs?");
+		macOsStudents = inputStudent.nextInt();
+		
 		totalNumberStudents = windowsStudents + linuxStudents + macOsStudents;
 		System.out.println("Total number of students: " + totalNumberStudents);
 		
-		float amountWindowsStudents = ((float)windowsStudents/totalNumberStudents) * 100;
-		float amountLinuxStudents = ((float)linuxStudents/totalNumberStudents) * 100;
-		float amountMacOsStudents = ((float)macOsStudents/totalNumberStudents) * 100;
+		double amountWindowsStudents = ((double)windowsStudents/totalNumberStudents) * 100;
+		double amountLinuxStudents = ((double)linuxStudents/totalNumberStudents) * 100;
+		double amountMacOsStudents = ((double)macOsStudents/totalNumberStudents) * 100;
 		
-		System.out.println("Windows students: " + (int)amountWindowsStudents + "%" );
-		System.out.println("Linux Students: " + (int)amountLinuxStudents + "%" );
-		System.out.println("MacOs Students:" + (int)amountMacOsStudents + "%" );
+		System.out.println( Math.round(amountWindowsStudents * 100.0)/100.0 + "%" + " of students use Windows" );
+		System.out.println( Math.round(amountLinuxStudents * 100.0)/100.0 + "%" + " of students use Linux" );
+		System.out.println( Math.round(amountMacOsStudents * 100.0)/100.0 + "%" + " of students use MacOs" );
+		
+		inputStudent.close();
 	}
 }
