1c072abcaab0cd7e23a170b7b5c254f76ab3c375
Bice93 <bianca.cerulo@hotmail.it>
2023-01-26 14:36:13 +0100

    Added CibiPreferiti

---
+++ src/org/lessons/java/security/CibiPreferiti.java
@@ -0,0 +1,19 @@
+package org.lessons.java.security;
+
+public class CibiPreferiti {
+	
+	public static void main(String[] args) {
+			
+		String[] foods = {"Fiorentina", "Lasagna", "Pizza", "Ramen", "Bibimbap", "Genovese", "Hamburger", "Calamarata"};
+		
+		//Stampo la lunghezza dell'array
+		System.out.println("La lunghezza dell'array è: " + foods.length );
+		
+		//Stampo il primo piatto
+		System.out.println("Il mio cibo preferito è: " + foods[0] );
+		
+		//Stampo l'ultimo piatto
+		System.out.println("Il mio cibo preferito ma non troppo: " + foods[7] );
+	}
+
+}
